22cdf29afd6bc2bdef448f046376694c6d9c91c0
andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>
2026-08-13 19:30:20 -0500

    fix: correct typo cudaSuccesAssert -> cudaSuccessAssert
    Signed-off-by: andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>

--- corelib/recsys_kvcache_manager/src/gpu_kvcache_manager_impl.cpp
+++ corelib/recsys_kvcache_manager/src/gpu_kvcache_manager_impl.cpp
@@ -23,8 +23,8 @@
 
 #include <utility>
 
-#define cudaCheck(ans) { cudaSuccesAssert((ans), __FILE__, __LINE__); }
-inline void cudaSuccesAssert(cudaError_t code, const char *file, int line, bool abort=true)
+#define cudaCheck(ans) { cudaSuccessAssert((ans), __FILE__, __LINE__); }
+inline void cudaSuccessAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
    if (code != cudaSuccess) 
    {
